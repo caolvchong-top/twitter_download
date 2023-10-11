f84ecdaa4d9b2072c6ac57eff1019b89135a7b18
caolvchong-top <1262927285@qq.com>
2023-10-12 01:19:40 +0800

    csv生成

---
+++ csv_gen.py
@@ -0,0 +1,27 @@
+import csv
+import time
+
+class csv_gen():
+    def __init__(self, save_path:str, user_name, screen_name, tweet_range) -> None:
+        self.f = open(f'{save_path}{screen_name}.csv', 'w', encoding='utf-8-sig', newline='')
+        self.writer = csv.writer(self.f)
+
+        self.writer.writerow([user_name, screen_name])
+        self.writer.writerow(['Tweet Range : ' + tweet_range])
+        main_par = ['Tweet Date', 'Display Name', 'User Name', 'Tweet URL', 'Media Type', 'Media URl', 'Saved Filename', 'Tweet Content']
+        self.writer.writerow(main_par)
+
+        pass
+
+    def csv_close(self):
+        self.f.close()
+
+    def stamp2time(self, msecs_stamp:int) -> str:
+        timeArray = time.localtime(msecs_stamp/1000)
+        otherStyleTime = time.strftime("%Y-%m-%d %H:%M", timeArray)
+        return otherStyleTime
+    
+    def data_input(self, main_par_info:list) -> None:   #数据格式参见 main_par
+        main_par_info[0] = self.stamp2time(main_par_info[0])    #传进来的是 int 时间戳, 故转换一下
+        self.writer.writerow(main_par_info)
+
